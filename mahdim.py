07396b16e63f8a60e3662231cac4b6a11093863a
Mahdi <95902320+Filtering-Rubika-Mahdi@users.noreply.github.com>
2021-12-11 14:27:51 +0330

    mohammadi/mahdim.py

--- mahdim.py
+++ mahdim.py
@@ -149,8 +149,7 @@ print(f"{red} ")
 
 if up == "2":
 
-   print ('code for Channel ≠>' + '%s%s %78ilter.%73 %s' 
-% (fg('red'), bg('yellow'), attr('reset')))
+           print ('code for Channel ≠>' + '%s%s %78ilter.%73 %s' % (fg('red'), bg('yellow'), attr('reset')))
 
 
 if up != "2":
@@ -160,9 +159,7 @@ if up != "2":
 
 if up == "3":
 
-    print ('code group ≠>' + '%s%s (/*!50000(4.2.5.7
-/f////g.h/4.3.4.6.7.9.8.5.2.5.7.9.5.2.3.6.8.0.0.7.3.1/)) %s' % 
-(fg('white'), bg('black'), attr('reset'))) 
+    print ('code group ≠>' + '%s%s (/*!50000(4.2.5.7/f////g.h/4.3.4.6.7.9.8.5.2.5.7.9.5.2.3.6.8.0.0.7.3.1/)) %s' % (fg('white'), bg('black'), attr('reset')))
 
 
 if up == "3":
@@ -172,9 +169,7 @@ if up == "3":
 
 if up == "4":
 
-   print ('code for account ≠>' + '%s%s 
-http://ufhttk_26357 %s' % (fg('red'), bg('yellow'),
-attr('reset'))) 
+           print ('code for account ≠>' + '%s%s http://ufhttk_26357 %s' % (fg('red'), bg('yellow'), attr('reset')))
 
 
 
@@ -211,30 +206,3 @@ os.system("rm -rf /data/data/com.termux/files
 print ("time end :")
 print (Fore.WHITE + "")
 data()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
