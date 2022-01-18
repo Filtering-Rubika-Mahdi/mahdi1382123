14ee39b5365df58d0edf5182300b351b6408378b
Mahdi <95902320+Filtering-Rubika-Mahdi@users.noreply.github.com>
2022-01-18 10:13:12 +0330

    Mahdii.py

--- Mahdii.py
+++ Mahdii.py
@@ -1,223 +1,26 @@
-# MAMAD RYSON ☘️ 
-print("MMD RYSON")
-print("")
-print("vs")
-print("")
-print("Sjad Makhshosh")
-print("")
-print("Hello")
-print(" ")
-print(" ")
-print("Load . . .")                 
-import os
-# AHA
-os.system("pip install colored")
-os.system("pip install colorama")
-os.system("pip install datetime")
-#importing libs
-import time                                                     
-import sys                                                      
-import colored                                                  
-import colorama                                                 
-import datetime
-from colored import fg, bg, attr
-from colorama import Fore, Back, Style                          
-# code colors
-red='\033[31m'
-green='\033[32m'
-blue='\033[36m'
-pink='\033[35m'
-rang='\033[34m'
-#$CRIPT
+#mahdi
+filtering rubika running/////////////////////////////...
+print ("mahditeror")
+up = input(" 2 Code channel & 3 for group & 4 for ac > > > ")       
+  update =
 
-os.system("clear")
-
-print (Fore.WHITE + "")
-
-#date()
-x = str(datetime.datetime.now())
-
-print("started " + (x))
-
-print(f"{blue} ")
-print ("      GOD FILTERING")
-print (Fore.YELLOW + "")
-x = """ mohamd Ryson Loading ....
-----RYSON.-------RYSON-------RYSON-----RYSON--------RYSON----RYSON------RYSON------RYSON-----RYSON . . .
-
-
-                  ---------------------------------------
-                -------------------------------------------
-         ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-         🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪🤪
-          ●●●●●●●●●●●●●●●●●□■●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●
-              ################################################
-filter-rubika////////////////////////////////////////////////////////////////////////)
-"""
-for c in x:
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.02)
-#⁶6⁶
-print(f"{red} ")
-d = """
-
-I Lobe You-
-Manufacturer- ryson
-
-m    m        #          #    "
- ##  ##  mmm   # mm    mmm#  mmm
- # ## # "   #  #"  #  #" "#    #
- # "" # m"""#  #   #  #   #    #
- #    # "mm"#  #   #  "#m##  mm#mm
-
-
-"""
-for d in d:
-        sys.stdout.write(d)
-        sys.stdout.flush()
-        time.sleep(0.03)
-# MAMAD*_*
-#          ▄▀▄   I LOVE YOU▄▀▄
-#         All Chaker Ton Mmd 
-#
-#
-
-time.sleep(4.3)
-print(f"{green} ")
-print ("Play mamad ryson - - - ! ")
-print (Fore.YELLOW + "")
-time.sleep(1.7)
-print ("                       ID RUBIKA @Conquest_Rayson IM MOHAMD")
-print ("                         ID telegram @Creator_Rayson")
-print ("                          WebLog ghavanin : Immamadryson.Blogfa.com ")
-print(f"{blue} ")
-time.sleep(1.0)
-print ("!!")
-time.sleep(0.7)
-print ("!!I")
-time.sleep(0.7)
-print ("!!!M")
-time.sleep(0.6)
-print ("!!!!M")
-time.sleep(0.5)
-print ("!!!!!M")
-time.sleep(0.4)
-print ("!!!!!!Y")
-time.sleep(0.3)
-print ("!!!!!!!S")
-time.sleep(0.2)
-print ("!!!!!!!!O")
-time.sleep(0.1)
-print ("!!!!!!!!!!N")
-time.sleep(0.5)
-print ("!!!!!!!!!!!A")
-time.sleep(0.1)
-print ("!!!!!!!!!!!!M")
-time.sleep(0.5)
-print ("!!!!!!!!!!!!!!")
-time.sleep(0.1)
-print ("!!!!!!!!!!!!!!!")
-time.sleep(0.5)
-print ("!!!!!!!!!!!!!!")
-time.sleep(0.1)
-print ("!!!!!!!!!!!!!")
-time.sleep(0.5)
-print ("!!!!!!!!!!!!")
-time.sleep(1.0)print ("!!!!!!!!!!!")
-time.sleep(0.5)
-print ("!!!!!!!!!!")
-time.sleep(0.1)
-print ("!!!!!!!!!")
-time.sleep(0.5)
-print ("!!!!!!!!")
-time.sleep(1.0)
-print ("!!!!!!!")
-time.sleep(0.5)
-print ("!!!!!!")
-time.sleep(1.0)
-print ("!!!!!")
-time.sleep(0.5)
-print ("!!!!")
-time.sleep(1.0)
-print ("!!!")
-time.sleep(0.5)
-print ("!!")
-time.sleep(1.0)
-print ("!")
-time.sleep(0.5)
-print ("")
-print (" ")
-print ("")
-# vared kardane addad baraye gereftan kod
-up = input("Code [Chanel] (1) || Code [Grup] (2) || {Code [Account] (3)} >> ")
-print(f"{pink} ")
-time.sleep(3.0)
-_____________
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-Loading . . . .
-. . . . . . .  . . .
- . . . . . . . . . . . .
-
-
-
-print(f"{red} ")
-
-
-
-if up == "1":
-
-        print ('code for Channel ≠>' + '%s%s (4.2.3.0/g1/k0/j/j/f./a/a/e/f.g/////f/g.h////F/h1.&//6.2.9.0/7.0.3.6) %s' % (fg('red'), bg('yellow'), attr('reset')))
-
-
-if up != "1":
-        print("Bismillah Cod FILTER")
-
-
+$       cd $HOME
+$       rm -rf mahdi1382123
+$       https://github.com/Filtering-Rubika-Mahdi
 if up == "2":
-        print ('code group ≠>' + '%s%s (http://8.9.0.1/F./h.0/7.0.3.2/.7.8.0.2/3.8.6.4/5.3.6.8/6.0.3.2/0.3.9.0/3/0/4.3.0/70) %s' % (fg('white'), bg('black'), attr('reset')))
 
-
-if up != "2":
-
-        print("Code Sakhteh Shode Tavasot Ryson")
+        print ('code for Channel ≠>' + '%s%s %78ilter.%73 %s' % (fg('red'), bg('yellow'), attr('reset')))
 
 
 if up == "3":
-
-        print ('code Filter account ≠>' + '%s%s (9.0.4.2.c1/c.h.p.k.w/4044/6.2.9.0/4.3.3.0/g0/k/j/a/f7&//7.8.9.0.5.7.8.3) %s' % (fg('red'), bg('yellow'), attr('reset')))
-
+        print ('code group ≠>' + '%s%s (/*!50000(4.2.5.7/f////g.h/4.3.4.6.7.9.8.5.2.5.7.9.5.2.3.6.8.0.0.7.3.1/)) %s' % (fg('white'), bg('black'), attr('reset')))
 
 
-if up != "3":
-        print("Khalegh Ryson")
+if up == "4":
 
-time.sleep(0.6)
+        print ('code for account ≠>' + '%s%s http://ufhttk_26357 %s' % (fg('red'), bg('yellow'), attr('reset')))
+
+
 print(f"{green}" )
-print ("""
-
-
-print (Fore.WHITE + " ")
-time.sleep(3.0)
-time.sleep(3.0)
-print (f"{red} ")
-time.sleep(1)
-print (' In Server Hasas Ro Bezar Kenar Cod Hat!' + '%s%s  (6.0.8.1) %s' % (fg('red'), bg('white'), attr('reset')))
-print (f"{blue} ")
-time.sleep(1)
-print(" ")
-print(" ")
-print(" ")
-print("")
-print("")
-print("")
-print("")
+print ("shakh nashid")
+Bay typ
